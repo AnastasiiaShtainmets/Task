03e0bbd6b3c2c5a8747990e2ec6e7505a7418dce
Анастасия <nastya589sht@gmail.com>
2022-02-17 21:20:26 +0300

    init commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void waterGlassesConvert(View view) {
+        counter++;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+
+}
