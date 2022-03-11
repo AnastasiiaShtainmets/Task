bcff19be4e106cb8482da56afa7ed749008fb991
Анастасия <nastya589sht@gmail.com>
2022-03-11 11:13:44 +0300

    Clipper

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -4,21 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<MyObject, myObj> extends AppCompatActivity {
     private Integer counter = 0;
 
+    public static final String TAG = "StartActivity";
+    private Object myObj;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
     public void waterGlassesConvert(View view) {
         counter++;
-        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        TextView counterView = (TextView) findViewById(R.id.txt_counter);
         counterView.setText(counter.toString());
+
     }
 
-}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count", counter);
+        Log.d(TAG, "onSaveInstanceState");
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("count")) {
+            counter = savedInstanceState.getInt("count");
+        }
+        Log.d(TAG, "onRestoreInstanceState");
+        resetUI();
+
+    }
+
+    private void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
+        Log.d(TAG, "resetUI");
+    }
+
+    }
+
+
